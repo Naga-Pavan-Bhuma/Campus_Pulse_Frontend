b0735b480a96d921ec6f50dbc81668348f72593b
Anirudh-654 <anirudhluky654@gmail.com>
2025-04-18 09:20:26 +0530

    Faculty Page added

--- campus_pulse_frontend/src/App.js
+++ campus_pulse_frontend/src/App.js
@@ -5,15 +5,19 @@ import LoginPage from './components/LoginPage';
 import Home from './components/Home';
 import Body from './components/Body';
 import ExamSchedule from './components/ExamSchedule';
+import MainLayout from './components/MainLayout';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<Body />}>
+        <Route path="/" element={<Body />}>
           <Route index element={<Home />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="signup" element={<SignupPage />} />
+          <Route path="examschedule" element={<ExamSchedule />} />
+        </Route>
+        <Route path="/student" element={<MainLayout />}>
           <Route path="examschedule" element={<ExamSchedule />} />
         </Route>
       </Routes>
--- campus_pulse_frontend/src/index.js
+++ campus_pulse_frontend/src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import React from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </GoogleOAuthProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
